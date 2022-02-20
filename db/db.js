0bd89ba1cc13170175e2878bd05400aba78626a2
Osim Khusainov <keedice@gmail.com>
2022-02-21 00:06:23 +0300

    pg url

--- db/db.js
+++ db/db.js
@@ -1,12 +1,17 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const pool = new Pool({
+const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
-});
+};
+const proConfig = {
+  connectionString: process.env.PG_URL,
+};
+
+const pool = new Pool(proConfig);
 
 module.exports = pool;
